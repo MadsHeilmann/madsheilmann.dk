652238c2d779ed0d70dff7f280568263cb808fef
Mads Heilmann <mads.heilmann@hotmail.com>
2024-04-11 18:07:14 +0200

    Resolve #1

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.all,
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: ["*.config.*", ".next/"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE", "StrictPascalCase"],
+        },
+      ],
+      "@typescript-eslint/prefer-readonly-parameter-types": [
+        "error",
+        {
+          allow: [
+            "$",
+            {
+              from: "package",
+              package: "react",
+              name: ["ChangeEvent", "MouseEvent", "ReactNode"],
+            },
+            {
+              from: "lib",
+              name: ["Request"],
+            },
+          ],
+        },
+      ],
+      "@typescript-eslint/no-restricted-imports": [
+        "error",
+        {
+          patterns: ["@mui/*/*/*"],
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from "next";
+import type { FC, ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "madsheilmann.dk",
   description: "Mads Heilmann's personal website",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export interface Children {
+  readonly children: ReactNode;
+}
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+const RootLayout: FC<Children> = ({ children }: Readonly<Children>) => {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
