c2f104e48867f7f6f6478244f8b0b80159a284be
Mads Heilmann <mads.heilmann@hotmail.com>
2024-04-11 18:07:14 +0200

    Resolve #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
-  return (
-    <main></main>
-  );
-}
+"use client";
+
+import type { FC, ReactElement } from "react";
+
+const Home: FC = (): ReactElement | null => {
+  return <main>Landing page</main>;
+};
+
+export default Home;
